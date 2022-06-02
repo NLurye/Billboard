c01ae9bea3ccd439c1eb68968907dc5416d9a65d
Anastasia Lurye <anastasia.lurye@gmail.com>
2022-06-02 23:36:56 +0300

    Task 2 Completed.

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
+//Anastasia Lurye ID: 332644848
+//Tom Zur ID: 208287516
 const express = require('express');
-//const html = require("./index.html");
 fs = require('fs');
-
     const app = express();
     app.get('/screen=:num', function(req, res){
-
         fs.readFile(__dirname +'/index.html', 'utf8', function (err,data) {
             replace =  "let Snum = "+ req.params.num;
             let result = data.replace(/let Snum = 0/g, replace);
-
             fs.writeFile(__dirname +'/index.html', result, 'utf8', function (err) {
-
-                res.sendFile(__dirname +'/index.html');
-                console.log(req.params.num);
-
+                res.sendFile(__dirname +'/index.html',function (){
+                    result = data.replace(/let Snum = 1/g || /let Snum = 2/g || /let Snum = 3/g, 'let Snum = 0');
+                    fs.writeFile(__dirname +'/index.html', result, 'utf8', function (err) {});
+                });
             });
         });
-
-
     });
-
     app.get('/templateA.html', function(req, res){
         res.sendFile(__dirname +'/templateA.html');
     });
@@ -29,5 +24,4 @@ fs = require('fs');
     app.get('/templateC.html', function(req, res){
         res.sendFile(__dirname +'/templateC.html');
     });
-
     app.listen(8080);
